d9bc22c1105a709973e07dfc4b3b389e1867d8f7
darshit-97 <darshit2468@gmail.com>
2025-06-11 14:32:56 +0530

    Add StudentGradingApplication

---
+++ CsharpProjects/StudentGradingApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Student Grading Application
+----------------------------
+This C# console application calculates and displays final grades for students based on their exam and extra credit scores.
+Each student has 5 exam scores (0–100), and optional extra credit assignments.
+Extra credit scores (if present) are identified by array length and weighted at 10% of a full exam score.
+The program calculates the average score, applies extra credit, and assigns a letter grade automatically.
+Uses arrays and foreach loops to handle students and scores for scalability and easier maintenance.
+Outputs each student’s name, final numeric grade, and corresponding letter grade in a formatted report.
+*/
