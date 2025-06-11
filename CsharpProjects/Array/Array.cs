ba2aa62d85ad8b4435ecb0d9264a921fae856e8b
darshit-97 <darshit2468@gmail.com>
2025-06-11 13:03:04 +0530

    Add Foreach

--- CsharpProjects/Array/Array.cs
+++ CsharpProjects/Array/Array.cs
@@ -25,3 +25,26 @@ fraudulentOrderIDs[0] = "A100";
 Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
 Console.WriteLine($"There are {fraudulentOrderIDs.Length} Fraudulent Orders to process.");
+
+/* 
+you work for a manufacturing company. The company needs you to complete an inventory of your warehouse to determine the number of products that are ready to ship. In addition to the total number of finished products, you need to report the number of finished products stored in each individual bin in your warehouse, along with a running total. This running total will be used to create an audit trail so you can double-check your work and identify "shrinkage".
+*/
+
+/* string[] names = { "Rowena", "Helga", "Godric", "Salazar" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+} */
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} (Running Total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
