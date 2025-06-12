c14e92da6805d5ad102a9432db0df7a451b00d0d
darshit-97 <darshit2468@gmail.com>
2025-06-12 19:19:06 +0530

    Update Loops

--- CsharpProjects/Loops/Program.cs
+++ CsharpProjects/Loops/Program.cs
@@ -9,7 +9,9 @@ for (int j = 0; j < names.Length; j++)
 
 foreach (var name in names) Console.WriteLine(name); */
 
-for (int i = 1; i < 101; i++)
+/* ------------------------------------------------------------- */
+
+/* for (int i = 1; i < 101; i++)
 {
     if ((i % 3 == 0) && (i % 5 == 0))
         Console.WriteLine($"{i} - FizzBuzz");
@@ -19,4 +21,62 @@ for (int i = 1; i < 101; i++)
         Console.WriteLine($"{i} - Buzz");
     else
         Console.WriteLine($"{i}");
+} */
+
+/* do-while statement to break when a certain random number is generated */
+
+/* Random random = new();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7); */
+
+/* while statement that iterates only while a random number is greater than some value */
+
+/* Random random = new Random();
+int current = random.Next(1, 11);
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
 }
+Console.WriteLine($"Last number: {current}"); */
+
+/* continue statement to step directly to the Boolean expression */
+
+/* Random random = new Random();
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7); */
+
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
+
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
