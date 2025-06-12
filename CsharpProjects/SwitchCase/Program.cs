bd8a9898bf7bbd62712aadf6dc3de801dfd1239f
darshit-97 <darshit2468@gmail.com>
2025-06-12 16:40:36 +0530

    Update SwitchCase

--- CsharpProjects/SwitchCase/Program.cs
+++ CsharpProjects/SwitchCase/Program.cs
@@ -1,4 +1,6 @@
-﻿int employeeLevel = 100;
+﻿using System.Collections;
+
+int employeeLevel = 100;
 string employeeName = "John Smith";
 
 string title = "";
@@ -21,3 +23,102 @@ switch (employeeLevel)
 }
 
 Console.WriteLine($"{employeeName}, {title}");
+
+/* --------------------------------------------------------- */
+
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+if (product[0] == "01")
+{
+    type = "Sweat shirt";
+} else if (product[0] == "02")
+{
+    type = "T-Shirt";
+} else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}
+else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+} else if (product[1] == "MN")
+{
+    color = "Maroon";
+} else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+} else if (product[2] == "M")
+{
+    size = "Medium";
+} else if (product[2] == "L")
+{
+    size = "Large";
+} else
+{
+    size = "One Size Fits All";
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat Shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat Pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "Other";
+        break;
+}
+Console.WriteLine($"Product: {size} {color} {type}");
