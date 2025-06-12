a8d81a8ec52c69b47c1d1e4086cdbcb18e893930
darshit-97 <darshit2468@gmail.com>
2025-06-12 14:45:14 +0530

    Add Business Access Logic

--- CsharpProjects/Proj3/Program.cs
+++ CsharpProjects/Proj3/Program.cs
@@ -12,11 +12,60 @@ In this challenge, you'll implement a conditional operator to simulate a "coin f
 
 /* Method 1 */
 
-// Random coin = new();
-// int flip = coin.Next(0, 2);
-// Console.WriteLine((flip == 0) ? "Heads" : "Tails");
+Random coin = new();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "Heads" : "Tails");
 
 /* Method 2 */
 
-Random coin = new();
-Console.WriteLine((coin.Next(0, 2) == 0) ? "Heads" : "Tails");
+Random coin2 = new();
+Console.WriteLine((coin2.Next(0, 2) == 0) ? "Heads" : "Tails");
+
+/* 
+Decision logic challenge
+
+In this challenge, you'll implement decision logic based on a series of business rules. The business rules specify the access that will be granted to users based on their role-based permissions and their career level. Code branches will display a different message to the user depending on their permissions and level.
+
+User Access Rules Summary:
+ - Admin users:
+   - Level > 55: "Welcome, Super Admin user."
+   - Level <= 55: "Welcome, Admin user."
+ - Manager users:
+   - Level >= 20: "Contact an Admin for access."
+   - Level < 20: "You do not have sufficient privileges."
+ - All other users: "You do not have sufficient privileges." 
+*/
+Random permission = new();
+Random number = new();
+
+string randomPermisson = permission.Next(1, 3) == 1 ? "Admin" : "Manager";
+int level = number.Next(0, 100);
+
+Console.WriteLine($"Permission: {randomPermisson}\nLevel: {level}");
+
+if (randomPermisson.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin User.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin User.");
+    }
+}
+else if (randomPermisson.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for Access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
