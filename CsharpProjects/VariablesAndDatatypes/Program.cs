833713de26b760f950e6c87c0d99f48848051502
darshit-97 <darshit2468@gmail.com>
2025-06-17 11:37:55 +0530

    Add functionality to extract quantity from HTML and replace trademark symbols

--- CsharpProjects/VariablesAndDatatypes/Program.cs
+++ CsharpProjects/VariablesAndDatatypes/Program.cs
@@ -242,7 +242,9 @@ Console.WriteLine($"\t  Sub Total: {subtotal:C}");
 Console.WriteLine($"\t      Tax: {taxPercentage:P2}");
 Console.WriteLine($"\t  Total Billed: {total:C}"); */
 
-string customerName = "Ms. Barros";
+/* --------------------------------------------------------------- */
+
+/* string customerName = "Ms. Barros";
 
 string currentProduct = "Magic Yield";
 int currentShares = 2975000;
@@ -271,4 +273,41 @@ comparisonMessage += newProduct.PadRight(20);
 comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
 comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
 
-Console.WriteLine(comparisonMessage);
+Console.WriteLine(comparisonMessage); */
+
+/* --------------------------------------------------------------- */
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+// Extract the "quantity"
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int quantityStart = input.IndexOf(openSpan) + openSpan.Length; // + length of <span> so index at end of <span> tag
+int quantityEnd= input.IndexOf(closeSpan);
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+// Set output to input, replacing the trademark symbol with the registered trademark symbol
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+// Remove the opening <div> tag
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+// Remove the closing </div> tag and add "Output:" to the beginning
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
