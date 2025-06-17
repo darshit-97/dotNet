25b0edcdfee72acc75091fd73f1a1b1b7c4abbce
darshit-97 <darshit2468@gmail.com>
2025-06-17 10:29:05 +0530

    Add functionality to reverse, join and split string arrays

--- CsharpProjects/VariablesAndDatatypes/Program.cs
+++ CsharpProjects/VariablesAndDatatypes/Program.cs
@@ -145,7 +145,7 @@ foreach (var pallet in pallets)
     Console.WriteLine($"-- {pallet}");
 } */
 
-string[] pallets = [ "B14", "A11", "B12", "A13" ];
+/* string[] pallets = [ "B14", "A11", "B12", "A13" ];
 Console.WriteLine("");
 
 Array.Clear(pallets, 0, 2);
@@ -174,4 +174,17 @@ Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
+} */
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = string.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
 }
