efdd1272ad55b3f30db475df606a88342d4ca3be
darshit-97 <darshit2468@gmail.com>
2025-06-18 18:21:03 +0530

    Demonstrate pass by value and reference

--- CsharpProjects/Methods/Program.cs
+++ CsharpProjects/Methods/Program.cs
@@ -162,7 +162,7 @@ void ValidateRange()
 /* ==================================================================== */
 /* ==================================================================== */
 
-int[] schedule = { 800, 1200, 1600, 2000 };
+/* int[] schedule = { 800, 1200, 1600, 2000 };
 DisplayAdjustedTime(schedule, 6, -6);
 
 void DisplayAdjustedTime(int[] times, int currentGMT, int newGMT)
@@ -186,13 +186,76 @@ void DisplayAdjustedTime(int[] times, int currentGMT, int newGMT)
         int newTime = (times[i] + diff) % 2400;
         Console.WriteLine($"{times[1]} -> {newTime}");
     }
+} */
+
+/* ==================================================================== */
+/* ==================================================================== */
+
+/* double pi = 3.14159;
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
+
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+} */
+
+/* ==================================================================== */
+/* ==================================================================== */
+
+/* Pass by Value */
+
+int a = 3;
+int b = 4;
+int c = 0;
+
+multiply(a, b, c);
+Console.WriteLine($"Global Statement: {a} x {b} = {c}");
+
+void multiply(int a, int b, int c)
+{
+    c = a * b;
+    Console.WriteLine($"Inside Multiply method: {a} x {b} = {c}");
 }
 
 /* ==================================================================== */
 /* ==================================================================== */
 
-/* ==================================================================== */
-/* ==================================================================== */
+/* Pass by Reference */
 
-/* ==================================================================== */
-/* ==================================================================== */
+int[] array = { 1, 2, 3, 4, 5 };
+
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    foreach (int a in array)
+    {
+        Console.Write($"{a}");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+}
