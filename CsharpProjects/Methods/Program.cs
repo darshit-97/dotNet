e5c2e4e0030adddd0e002a878ad90f6c9062a511
darshit-97 <darshit2468@gmail.com>
2025-06-19 14:53:53 +0530

    Add method to demonstrate Array Return type

--- CsharpProjects/Methods/Program.cs
+++ CsharpProjects/Methods/Program.cs
@@ -452,7 +452,7 @@ string ReverseSentence(string input1)
 
 /* Method that Return Boolean */
 
-string[] words = { "racecar", "talented", "deified", "tent", "tenet" };
+/* string[] words = { "racecar", "talented", "deified", "tent", "tenet" };
 
 Console.WriteLine("Is it a palindrome?");
 foreach (string word in words)
@@ -476,7 +476,41 @@ bool IsPalindrome(string word)
     }
 
     return true;
-}
+} */
 
 /* ==================================================================== */
 /* ==================================================================== */
+
+/* Return Arrays from Methods */
+
+/* int target = 60;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
+{
+    Console.WriteLine("No two coins make change");
+}
+else
+{
+    Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+}
+
+int[] TwoCoins(int[] coins, int target)
+{
+    for (int curr = 0; curr < coins.Length; curr++)
+    {
+        // Console.WriteLine($"Current Object: {coins[curr]}");
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            // Console.WriteLine($"Current Next Object: {coins[next]}");
+            // Console.WriteLine($"Current Object + Current Next Object: {coins[curr]} + {coins[next]} == {coins[curr]+coins[next]}");
+            // Console.WriteLine("--------------------------------------------");
+            if (coins[curr] + coins[next] == target)
+            {
+                return new int[] { curr, next };
+            }
+        }
+    }
+    return new int[0]; 
+} */
